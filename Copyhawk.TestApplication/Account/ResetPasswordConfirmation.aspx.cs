76807e243a83a4b3fe2c889dc1d9f2011a8e5b41
Jose Alava <jalava85@gmail.com>
2016-01-03 00:48:32 -0500

    Initial commit.

---
+++ Copyhawk.TestApplication/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Copyhawk.TestApplication.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Copyhawk.TestApplication/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Copyhawk.TestApplication.Startup))]
+namespace Copyhawk.TestApplication
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
